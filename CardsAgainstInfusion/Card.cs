3d192264754042f9df6c97eb87b839e720e2b32e
marcin.r.slowinski <marcin.r.slowinski@WIN-0K8RM2M4ZJ>
2018-01-23 16:29:25 -0500

    Inital version of Card class.

--- CardsAgainstInfusion/Card.cs
+++ CardsAgainstInfusion/Card.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static CardsAgainstInfusion.Model;
 
 namespace CardsAgainstInfusion
 {
     class Card
     {
+        Card(TypeOfCard color, string cardText, List<string> tags, string playedBy)
+        {
+            Color = color;
+            CardText = cardText;
+            Tags = tags;
+            PlayedBy = playedBy;
+        }
+        public TypeOfCard Color { get; set; }
+        public string CardText { get; set; }
+        private List<string> _tags;
+
+        public List<string> Tags
+        {
+            get { return _tags; }
+            set { _tags = value; }
+        }
+
+        public string PlayedBy { get; set; }
     }
 }
+//*enum TypeOfCard
+//Blue
+//White
+//*string CardText
+//This is the text related to the type of card(e.g: White has a word or words, Blue has an incomplete sentence)
+//*List<string> Tags
+//This is a list of words associated with the white cards.
+//Blue cards will have an empty list.
+//*string PlayedBy
+//The name of the AI who played this card so we know who to give credit to when it is chosen as the winner.
+//Placeholder for storing metrics - TBD
